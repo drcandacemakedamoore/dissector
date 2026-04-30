cea5bf6fc0d62d5de7caff5fea8c759a48b3d198
Candace Moore <doctormakeda@gmail.com>
2026-04-30 18:42:32 +0200

    cleanup

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -18,7 +18,6 @@ def test_extract_boundary_2d():
     assert sum(sum(extracted)) == 14
 
 
-
 def test_extract_boundary_3d():
     """example 3d boundary extract"""
     vol = np.zeros((10, 10, 10), dtype=int)
@@ -37,16 +36,3 @@ def test_extract_boundary_3d():
 
     assert sum(sum(sum(extracted3))) == 82
 
-# vol = np.zeros((10, 10, 10), dtype=int)
-
-
-# for z in range(10):
-#     vol[5, 5, z] = 1
-#     vol[5, 6, z] = 1
-#     vol[5, 7, z] = 1
-#     vol[6, 5, z] = 1
-#     vol[6, 6, z] = 1
-#     vol[6, 7, z] = 1
-#     vol[7, 5, z] = 1
-#     vol[7, 6, z] = 1
-#     vol[7, 7, z] = 1
